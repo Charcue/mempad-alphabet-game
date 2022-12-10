355f58d249e31e5b62fbbd7b7ab367403702f589
Charcue <charfu@proton.me>
2022-12-09 23:01:39 -0500

    Change time.sleep to 't'

--- flash_number.py
+++ flash_number.py
@@ -7,6 +7,7 @@ def flashNumber(orig,choice,c_list):
     os.system('cls' if os.name == 'nt' else 'clear')
     print(c_list)
     c_list_a = c_list
+    t = 0.2
     btime = range(10)
     for i in btime:
         # clear the screen
@@ -14,27 +15,27 @@ def flashNumber(orig,choice,c_list):
         c_list_a[orig] = str(choice)
         print(c_list_a)
         # c_list_a=clist[choice]= "*"
-        time.sleep(0.1)
+        time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = "-"
         print(c_list_a)
-        time.sleep(0.1)
+        time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = " "
         print(c_list_a)
-        time.sleep(0.1)
+        time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = "|"
         print(c_list_a)
-        time.sleep(0.1)
+        time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = choice
         print(c_list_a)
-        time.sleep(0.1)
+        time.sleep(t)
 
 test_c_list = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
 flashNumber(2,"A",test_c_list)
