f0a80fb18f2be222335791e16f3526297528d757
Charcue <charfu@proton.me>
2022-12-09 22:44:29 -0500

    Added orig variable and letter to  final flash

--- flash_number.py
+++ flash_number.py
@@ -2,7 +2,7 @@ import os
 import time
 import draw_screen_pick
 
-def flashNumber(choice,c_list):
+def flashNumber(orig,choice,c_list):
     # clear the screen
     os.system('cls' if os.name == 'nt' else 'clear')
     print(c_list)
@@ -11,25 +11,30 @@ def flashNumber(choice,c_list):
     for i in btime:
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
-        c_list_a[choice] = str(choice)
+        c_list_a[orig] = str(choice)
         print(c_list_a)
         # c_list_a=clist[choice]= "*"
         time.sleep(0.1)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
-        c_list_a[choice] = "-"
+        c_list_a[orig] = "-"
         print(c_list_a)
         time.sleep(0.1)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
-        c_list_a[choice] = " "
+        c_list_a[orig] = " "
         print(c_list_a)
         time.sleep(0.1)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
-        c_list_a[choice] = "|"
+        c_list_a[orig] = "|"
+        print(c_list_a)
+        time.sleep(0.1)
+        # clear the screen
+        os.system('cls' if os.name == 'nt' else 'clear')
+        c_list_a[orig] = choice
         print(c_list_a)
         time.sleep(0.1)
 
 test_c_list = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-flashNumber(2,test_c_list)
+flashNumber(2,"A",test_c_list)
