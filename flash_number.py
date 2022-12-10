32413c99c61658697bb3205de0f7a62d2626b964
Charcue <charfu@proton.me>
2022-12-10 00:34:27 -0500

    change number of flashes and flash time number

--- flash_number.py
+++ flash_number.py
@@ -7,35 +7,35 @@ def flashNumber(orig,choice,c_list):
     os.system('cls' if os.name == 'nt' else 'clear')
     print(c_list)
     c_list_a = c_list
-    t = 0.2
-    btime = range(10)
+    t = 0.5
+    btime = range(1)
     for i in btime:
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = str(choice)
-        print(c_list_a)
+        draw_screen_pick.print_pick(c_list_a)
         # c_list_a=clist[choice]= "*"
         time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = "-"
-        print(c_list_a)
+        draw_screen_pick.print_pick(c_list_a)
         time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = " "
-        print(c_list_a)
+        draw_screen_pick.print_pick(c_list_a)
         time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = "|"
-        print(c_list_a)
+        draw_screen_pick.print_pick(c_list_a)
         time.sleep(t)
         # clear the screen
         os.system('cls' if os.name == 'nt' else 'clear')
         c_list_a[orig] = choice
-        print(c_list_a)
+        draw_screen_pick.print_pick(c_list_a)
         time.sleep(t)
 
-test_c_list = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-flashNumber(2,"A",test_c_list)
+# test_c_list = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
+# flashNumber(2,"A",test_c_list)
